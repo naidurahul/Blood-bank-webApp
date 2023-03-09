009ce66551150a2b4134aed5fa96f6bb29c07442
samrat <samratojha16@gmail.com>
2023-03-09 07:24:26 +0545

    -ui change

--- src/components/Dashboard/BloodStocks.jsx
+++ src/components/Dashboard/BloodStocks.jsx
@@ -1,13 +1,70 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import { PenFill } from "react-bootstrap-icons";
-import AddOrEditBloodCamps from "./AddOrEditModal";
+import { toast } from "react-toastify";
+import { addOrUpdateItemInArray } from "../../global/constants";
 import EditBloodStock from "./EditBloodStock";
 
 const bloodGroup = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
 
 const BloodStocks = () => {
   const [openEditBloodStock, setOpenEditBloodStock] = useState(null);
+  const [bloodStocks, setBloodStocks] = useState([]);
+  const [fetchingData, setFetchingData] = useState(false);
+  console.log(bloodStocks, "bloodStocks");
+  const onFormSubmit = (data) => {
+    if (data.mode === "add") {
+      addBloodStock(data.data);
+    } else {
+      editBloodStock(data.data);
+    }
+  };
+
+  const addBloodStock = async (details) => {
+    try {
+      const { data } = await axios.post(`http://localhost:4000/api/v1/stock`, {
+        ...details,
+      });
+      if (data.status) {
+        toast.success("Succesfully Added");
+        setBloodStocks([...addOrUpdateItemInArray(bloodStocks, data.msg)]);
+        setOpenEditBloodStock(null);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const editBloodStock = async (details) => {
+    try {
+      const { data } = await axios.put(`http://localhost:4000/api/v1/stock`, {
+        ...details,
+      });
+      if (data.status) {
+        console.log(data.msg);
+        setBloodStocks([...addOrUpdateItemInArray(bloodStocks, data.msg)]);
+        toast.success("Succesfully Edited");
+        setOpenEditBloodStock(null);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const fetchData = async () => {
+    try {
+      setFetchingData(true);
+      const { data } = await axios.get("http://localhost:4000/api/v1/stock");
+      setBloodStocks(data.msg);
+      setFetchingData(false);
+    } catch (error) {
+      console.log(error.message);
+      setFetchingData(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <>
       <div>
@@ -26,11 +83,18 @@ const BloodStocks = () => {
                     <h6>{bg}</h6>{" "}
                     <PenFill
                       className="text-green hover"
-                      onClick={() => setOpenEditBloodStock({ bg })}
+                      onClick={() =>
+                        setOpenEditBloodStock(
+                          bloodStocks?.find((x) => x.bg === bg)
+                        )
+                      }
                     />
                   </div>
                   <div className="mt-2">
-                    <h6>Stock Available :</h6>
+                    <h6>
+                      Stock Available :
+                      {bloodStocks?.find((x) => x.bg === bg)?.stock ?? "N/A"}{" "}
+                    </h6>
                     <h6>Donor Registered({bg}) :</h6>
                     <h6>Available :</h6>
                   </div>
