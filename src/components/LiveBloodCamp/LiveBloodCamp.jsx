a71298bc17d8f8d26af775dd82a1b3022e6149b8
samrat <samratojha16@gmail.com>
2023-04-24 01:39:06 +0545

    -bf

--- src/components/LiveBloodCamp/LiveBloodCamp.jsx
+++ src/components/LiveBloodCamp/LiveBloodCamp.jsx
@@ -76,91 +76,93 @@ const LiveBloodCamp = () => {
   }, [searchQuery, bloodCamps]);
   return (
     <>
-      <h6 className="huge mb-0 text-dark mt-3 ">
-        Make a Difference Today,
-        <h6 className="xxxxlarge mb-0 text-dark d-flex ">
-          Find Blood Donation Camps in Nepal and Save Lives
-          <Search size={40} className="ml-2 mt-1 text-primary" />
+      <div style={{ height: fetchingDate ? "80vh" : "auto" }}>
+        <h6 className="huge mb-0 text-dark mt-3 ">
+          Make a Difference Today,
+          <h6 className="xxxxlarge mb-0 text-dark d-flex ">
+            Find Blood Donation Camps in Nepal and Save Lives
+            <Search size={40} className="ml-2 mt-1 text-primary" />
+          </h6>
         </h6>
-      </h6>
-      <div className="d-flex my-3 flex-row-reverse">
-        <div className="w-100 d-flex flex-row-reverse">
-          <Form className="my-1 w-50">
-            <Form.Control
-              placeholder="Search camps by location.."
-              value={searchQuery}
-              onChange={(e) => setSearchQUery(e.target.value)}
-            ></Form.Control>
-          </Form>
+        <div className="d-flex my-3 flex-row-reverse">
+          <div className="w-100 d-flex flex-row-reverse">
+            <Form className="my-1 w-50">
+              <Form.Control
+                placeholder="Search camps by location.."
+                value={searchQuery}
+                onChange={(e) => setSearchQUery(e.target.value)}
+              ></Form.Control>
+            </Form>
+          </div>
         </div>
-      </div>
-      {fetchingDate ? (
-        <Loader />
-      ) : (
-        <div>
-          <Row>
-            {!filteredList?.length ? (
-              <h6 className="mt-5 w-100 d-flex xxxlarge justify-content-center">
-                <DatabaseX
-                  size={50}
-                  className="mr-3"
-                  style={{ marginTop: "-20px" }}
-                />{" "}
-                No Data to Show!
-              </h6>
-            ) : (
-              filteredList?.map((camp, index) => {
-                return (
-                  <Col xs={12} md={6} lg={6}>
-                    <Card className="blood-camp-card my-2">
-                      <Card.Body className="d-flex justify-content-between">
-                        <div>
-                          <Card.Title className="blood-camp-card-title d-flex justify-content-between">
-                            {camp.cName}
-                          </Card.Title>
-                          <Card.Subtitle className="mb-2 text-muted">
-                            {camp?.address}{" "}
-                          </Card.Subtitle>
-                          <Card.Text className="blood-camp-card-details tiny">
-                            <div>
-                              <strong>Date:</strong>{" "}
-                              {moment(camp.sDate).format("MMM Do YYYY")}
-                              <br />
-                              <strong>Time:</strong>{" "}
-                              {moment(camp.sDate).format("HH:mm a")}
-                              <br />
-                              {camp?.description}
-                            </div>
-                          </Card.Text>
-                          <Button
-                            variant="outline-green d-flex border-dark"
-                            onClick={() => setOpenParticipantModal(camp)}
-                          >
-                            Participate <ArrowRight className="mt-1 ml-2" />
-                          </Button>{" "}
-                        </div>{" "}
-                        <Alert
-                          variant="green"
-                          className="d-flex px-1 pt-1 pb-0"
-                          style={{ height: 60 }}
-                        >
-                          <PersonCheckFill size={40} className="mr-2" />{" "}
+        {fetchingDate ? (
+          <Loader />
+        ) : (
+          <div>
+            <Row>
+              {!filteredList?.length ? (
+                <h6 className="mt-5 w-100 d-flex xxxlarge justify-content-center">
+                  <DatabaseX
+                    size={50}
+                    className="mr-3"
+                    style={{ marginTop: "-20px" }}
+                  />{" "}
+                  No Data to Show!
+                </h6>
+              ) : (
+                filteredList?.map((camp, index) => {
+                  return (
+                    <Col xs={12} md={6} lg={6}>
+                      <Card className="blood-camp-card my-2">
+                        <Card.Body className="d-flex justify-content-between">
                           <div>
-                            <h6 className="xxlarge mb-0">Participants</h6>
-                            <h6 className="xxxlarge mb-0">
-                              {camp?.donorRegistered?.length}
-                            </h6>
-                          </div>
-                        </Alert>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                );
-              })
-            )}
-          </Row>
-        </div>
-      )}
+                            <Card.Title className="blood-camp-card-title d-flex justify-content-between">
+                              {camp.cName}
+                            </Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">
+                              {camp?.address}{" "}
+                            </Card.Subtitle>
+                            <Card.Text className="blood-camp-card-details tiny">
+                              <div>
+                                <strong>Date:</strong>{" "}
+                                {moment(camp.sDate).format("MMM Do YYYY")}
+                                <br />
+                                <strong>Time:</strong>{" "}
+                                {moment(camp.sDate).format("HH:mm a")}
+                                <br />
+                                {camp?.description}
+                              </div>
+                            </Card.Text>
+                            <Button
+                              variant="outline-green d-flex border-dark"
+                              onClick={() => setOpenParticipantModal(camp)}
+                            >
+                              Participate <ArrowRight className="mt-1 ml-2" />
+                            </Button>{" "}
+                          </div>{" "}
+                          <Alert
+                            variant="green"
+                            className="d-flex px-1 pt-1 pb-0"
+                            style={{ height: 60 }}
+                          >
+                            <PersonCheckFill size={40} className="mr-2" />{" "}
+                            <div>
+                              <h6 className="xxlarge mb-0">Participants</h6>
+                              <h6 className="xxxlarge mb-0">
+                                {camp?.donorRegistered?.length}
+                              </h6>
+                            </div>
+                          </Alert>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  );
+                })
+              )}
+            </Row>
+          </div>
+        )}
+      </div>
       <Participant
         openParticipantModal={openParticipantModal}
         handleClose={() => setOpenParticipantModal(null)}
